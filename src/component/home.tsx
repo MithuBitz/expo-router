984c301b83b02cecdd655513b3d8206a32bf0ebb
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2026-05-21 16:09:50 +0530

    Implement useRouter from expo-router

--- src/component/home.tsx
+++ src/component/home.tsx
@@ -1,8 +1,10 @@
-import { Link } from "expo-router";
+
+import { Link, useRouter } from "expo-router";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 const Home = () => {
+  const router = useRouter();
   return (
     <View>
       <Text>Edit src/app/index.tsx to edit this screen.</Text>
@@ -10,7 +12,8 @@ const Home = () => {
       <Link href="/profile/details">Profile Details</Link>
       <Link href="/user/145">UserId Page</Link>
       <Link href="/username/mithu">Username Page</Link>
-      <Link href="/docs/react">React Docs</Link>
+      {/* <Link href="/docs/react">React Docs</Link> */}
+      <Button title="Go to Docs" onPress={() => router.navigate('/docs/react')}/>
       <Link href="/docs/react/introduction">React Docs Subtopics</Link>
     </View>
   );
